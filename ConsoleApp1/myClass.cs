032f0a75140be6d6e576b4380c6c05cad85a6302
Krishna-Lv <krishnakumar.s@logicvalley.in>
2021-10-13 17:24:37 +0530

    Added arraylist method by krishna

--- ConsoleApp1/myClass.cs
+++ ConsoleApp1/myClass.cs
@@ -4,5 +4,23 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+        public void Arraylist()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Insert(0, 3);
+            arrayList.Add(4);
+            arrayList.Add(50);
+            Console.WriteLine(arrayList.Count);
+            Console.WriteLine(arrayList.Capacity);
+            arrayList.Reverse();
+            arrayList.Contains(50);
+            Console.WriteLine();
+            foreach (int item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
 }
